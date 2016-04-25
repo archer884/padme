1fcf4851c4cb502e5bdd754722b8b35f6be1b263
J/A <archer884@gmail.com>
2016-04-25 15:46:24 -0500

    Trim whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,15 @@ impl<T: AsRef<str>> Pad for T {
     fn pad_left(&self, length: usize, padding_character: char) -> String {
         let padding_length = get_padding_length(self.as_ref(), length);
         let padding = get_padding(padding_character, padding_length);
-        
+
         padding + self.as_ref()
     }
-    
+
     fn pad_right(&self, length: usize, padding_character: char) -> String {
         let padding_length = get_padding_length(self.as_ref(), length);
         let padding = get_padding(padding_character, padding_length);
 
-        self.as_ref().to_owned() + &padding        
+        self.as_ref().to_owned() + &padding
     }
 }
 
@@ -30,19 +30,19 @@ fn get_padding_length(content: &str, length: usize) -> usize {
 
 fn get_padding(padding_character: char, length: usize) -> String {
     unsafe {
-        String::from_utf8_unchecked(vec![padding_character as u8; length])        
+        String::from_utf8_unchecked(vec![padding_character as u8; length])
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::Pad;
-    
+
     #[test]
     fn pad_left_works() {
         assert_eq!("    1", "1".pad_left(5, ' '));
     }
-    
+
     #[test]
     fn pad_right_works() {
         assert_eq!("1    ", "1".pad_right(5, ' '));
